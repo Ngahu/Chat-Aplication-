037f35e926cf4713435c8cfacdfcc23da68a34cd
Ngahu <jamaalaraheem@gmail.com>
2016-09-07 10:28:46 +0300

    addig of the downloaded added files

--- app/server.py
+++ app/server.py
@@ -4,6 +4,7 @@ monkey.patch_all()
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import SocketIO, emit, join_room
 
+
 app = Flask(__name__)
 app.debug=True
 app.config['SECRET_KEY'] = 'nuttertools'
